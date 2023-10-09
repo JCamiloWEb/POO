63eb467e04b1ceeaf8f4bc9607365392afa92cb4
JNCamil <juancamvh@hotmail.com>
2023-10-09 23:31:38 +0200

    POO

--- clases_metodos_propiedades/herencia/clases.php
+++ clases_metodos_propiedades/herencia/clases.php
@@ -156,7 +156,7 @@ class TecnicosRedes extends Informatico{
         $this->experienciaRedes=5;
     }
     public function auditarRedes(){
-        return "auditanto redes";
+        return "auditanto las redes";
 
     }
 
